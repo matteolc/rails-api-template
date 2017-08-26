e990259b96870b9f4f83193ac5b3fc328818b81f
matteolc <root@daddy-monkey.voxbox.io>
2017-08-26 19:12:59 +0200

    Updates

--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -1,10 +1,12 @@
 p "Creating users.."
-admin = User.create password: ENV['ADMIN_SECRET'],
+admin = User.create email: Faker::Internet.email,
+                    password: ENV['ADMIN_SECRET'],
                     username: 'admin'                 
                    
 admin.add_role :admin
 
-user = User.create password: ENV['USER_SECRET'],
+user = User.create email: Faker::Internet.email,
+                   password: ENV['USER_SECRET'],
                    username: 'user'
                    
 user.add_role :user
