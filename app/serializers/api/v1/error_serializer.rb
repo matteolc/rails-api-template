73a8998c9e7e7d5ccd311422ca0aa9a3aba0a941
matteolc <root@daddy-monkey.voxbox.io>
2017-08-26 18:57:32 +0200

    Updates

--- app/serializers/api/v1/error_serializer.rb
+++ app/serializers/api/v1/error_serializer.rb
@@ -1,5 +1,6 @@
 class Api::V1::ErrorSerializer
-  UNKNOWN_ERROR = 'Something went wrong, no more info is available unforunately!'.freeze
+
+  UNKNOWN_ERROR = 'Something went wrong!'.freeze
   DEFAULT_POINTER = 'data'.freeze
 
   def initialize(status, errors)
@@ -11,74 +12,62 @@ class Api::V1::ErrorSerializer
     end
   end
 
-  def as_json
-    {
-      errors: errors
-    }
-  end
+  def as_json() { errors: errors } end
 
-  def to_json
-    as_json.to_json
-  end
+  def to_json() as_json.to_json end
 
   private
 
-    def parse_am_errors(errors)
-      error_messages = errors.full_messages
-
-      errors.map.with_index do |(k, v), i|
-        ErrorDecorator.new(k, v, error_messages[i])
-      end
+  def parse_am_errors(errors)
+    error_messages = errors.full_messages
+    errors.map.with_index do |(k, v), i|
+      ErrorDecorator.new(k, v, error_messages[i])
     end
+  end
 
-    def errors
-      @errors.map do |error|
-        {
-          status: @status,
-          title: normalize_title(error),
-          detail: normalize_error(error),
-          source: {
-            pointer: error_pointer(error)
-          }
+  def errors
+    @errors.map do |error|
+      {
+        status: @status,
+        title: normalize_title(error),
+        detail: normalize_error(error),
+        source: {
+          pointer: error_pointer(error)
         }
-      end
+      }
+    end
+  end
+
+  def normalize_title(error)
+    error.try(:title) || error.try(:to_s) || UNKNOWN_ERROR
+  end
+
+  def normalize_error(error)
+    error.try(:details) || error.try(:to_s) || UNKNOWN_ERROR
+  end
+
+  def error_pointer(error)
+    if error.respond_to?(:pointer)
+      return error.pointer
+    else
+      return DEFAULT_POINTER
+    end
+  end
+
+  class ErrorDecorator
+
+    def initialize(key, value, message)
+      @key, @value, @message = key, value, message
     end
 
-    def normalize_title(error)
-      error.try(:title) || error.try(:to_s) || UNKNOWN_ERROR
-    end
+    def title() "#{@key.to_s.titleize} #{@value}" end
 
-    def normalize_error(error)
-      error.try(:details) || error.try(:to_s) || UNKNOWN_ERROR
-    end
+    def details() @value end
 
-    def error_pointer(error)
-      if error.respond_to?(:pointer)
-        return error.pointer
-      else
-        return DEFAULT_POINTER
-      end
-    end
+    def to_s() @message end
 
-    class ErrorDecorator
-      def initialize(key, value, message)
-        @key, @value, @message = key, value, message
-      end
+    def pointer() "data/attributes/#{@key.to_s}" end
 
-      def title
-        "#{@key.to_s.titleize} #{@value}"
-      end
-
-      def details
-        @value
-      end
-
-      def to_s
-        @message
-      end
-
-      def pointer
-        "data/attributes/#{@key.to_s}"
-      end
-    end
+  end
+  
 end
--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -1,32 +1,3 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id                 :uuid             not null, primary key
-#  provider           :string           default("email"), not null
-#  uid                :string           default(""), not null
-#  username           :string           not null
-#  encrypted_password :string           default(""), not null
-#  sign_in_count      :integer          default(0), not null
-#  current_sign_in_at :datetime
-#  last_sign_in_at    :datetime
-#  current_sign_in_ip :string
-#  last_sign_in_ip    :string
-#  name               :string
-#  nickname           :string
-#  image              :string
-#  email              :string
-#  tokens             :json
-#  created_at         :datetime         not null
-#  updated_at         :datetime         not null
-#
-# Indexes
-#
-#  index_users_on_email                  (email)
-#  index_users_on_uid_and_provider       (uid,provider) UNIQUE
-#  index_users_on_username_and_provider  (username,provider) UNIQUE
-#
-
 module Api::V1
   class UserSerializer < ActiveModel::Serializer
   	
