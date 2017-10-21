dff333361e05865a765fab11db37995951ecac34
matteolc <root@daddy-monkey.voxbox.io>
2017-10-21 19:07:27 +0200

    Make it work

--- lib/templates/active_record/model/model.rb
+++ lib/templates/active_record/model/model.rb
@@ -1,9 +1,7 @@
 <% module_namespacing do -%>
-    class <%= class_name %> < <%= parent_class_name.classify %>
-        <% attributes.select(&:reference?).each do |attribute| -%>
-        belongs_to :<%= attribute.name %><%= ', polymorphic: true' if attribute.polymorphic? %><%= ', required: true' if attribute.required? %>
-        <% end -%>
-        include PgSearch      
-        pg_search_scope :search, against: column_names, using: { tsearch: { prefix: true, normalization: 2 } }    
-    end
+class <%= class_name %> < <%= parent_class_name.classify %>
+    <% attributes.select(&:reference?).each do |attribute| -%>
+    belongs_to :<%= attribute.name %><%= ', polymorphic: true' if attribute.polymorphic? %><%= ', required: true' if attribute.required? %>
+    <% end -%>    
+end
 <% end -%>
